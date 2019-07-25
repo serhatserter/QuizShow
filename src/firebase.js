f1b8bf2ce3bf7c11db5c43d2a93b3e2c575fab5a
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2019-07-25 12:55:42 +0300

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import Firebase from 'firebase'
+  const firebaseApp = Firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+  })
+export const db = firebaseApp.database()
